9fa4277322400bdcde0253e0234e6ff634838aaa
millienytorressanta <tmillieny@gmail.com>
2024-04-16 11:07:30 -0400

    back-end

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
-"""Documentation"""
+"""Dictionary"""
 import json
 import requests
 
 api_users_url = 'https://jsonplaceholder.typicode.com/users'
 api_todos_url = 'https://jsonplaceholder.typicode.com/todos'
 
-# Fetch user data
 response = requests.get(api_users_url)
 users = response.json()
 
-# Extract user IDs
-user_ids = [user['id'] for user in users]
+user_ids = []
+for user in users:
+    user_ids.append(user["id"])
 
-# Create a session for efficient HTTP requests
-with requests.Session() as session:
-    json_dict = {}
+json_dict = {}
 
-    for idx, user in enumerate(users, 1):
-        # Fetch user data and todos in a single request
-        response = session.get(f"{api_users_url}/{user['id']}", params={'_embed': 'todos'})
-        if response.status_code != 200:
-            print(f"Failed to fetch data for user {user['id']}")
-            continue
-        
-        user_data = response.json()
-        todos = user_data.get('todos', [])
-        
-        # Extract relevant information and construct JSON data
-        json_dict[user['id']] = [{'username': user_data['username'], 'task': todo['title'], 'completed': todo['completed']} for todo in todos]
+for employee_id in user_ids:
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(api_url)
 
-# Write data to JSON file
+    employee_name = response.json()["username"]
+
+    api_url2 = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+        )
+    response = requests.get(api_url2)
+
+    tasks = response.json()
+
+    json_dict[employee_id] = []
+    for task in tasks:
+        json_format = {
+            "username": employee_name,
+            "task": task["title"],
+            "completed": task["completed"]
+        }
+        json_dict[employee_id].append(json_format)
+
 with open("todo_all_employees.json", 'w') as json_file:
     json.dump(json_dict, json_file)
